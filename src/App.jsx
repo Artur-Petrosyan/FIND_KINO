564d71177b22da45c51dfa89cf2e6074e8d6e953
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-10-31 23:37:50 +0400

    feat: router
    using BrowserRouter for handling client-side routing in the application,
    add created the routes '/' , '/products'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Header from "./components/Header/Header";
+import { Routes, Route } from 'react-router-dom'
 import '../src/styles/reset.scss'
 import '../src/styles/common.scss'
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+
     </div>
   );
 }
