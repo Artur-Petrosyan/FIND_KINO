8e72e7ae27df0250a0d6154bbc19f7fa502a3a04
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-10-31 20:56:49 +0400

    build: package.json
    installed the classnames,react-router-dom,scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header className='header'>
+            <nav className='header__nav'>
+                  <ul className='nav__ul'>
+                    <li className='nav__li'>Home</li>
+                    <li className='nav__li'>Products</li>
+                  </ul>
+            </nav>
+        </header>
     );
 };
 
