6781594ed55833f7e7b4e4065b28162e19cc8737
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-01 19:06:30 +0400

    feat: get params
    created get request for pagination

--- src/api/http-api.js
+++ src/api/http-api.js
@@ -1,9 +1,16 @@
 import { BASE_URL } from "../constants/api"
 
 export const createGetRequest = async (endpoint) => {
-    const response = await fetch(`${BASE_URL}${endpoint}`,{
-        method : "GET",
+    const response = await fetch(`${BASE_URL}${endpoint}`, {
+        method: "GET",
     })
+    const data = await response.json()
+    return data
+}
+
+
+export const createGetRequsetWithParams = async (endpoint, offset, limit) => {
+    const response = await fetch(`${BASE_URL}${endpoint}/?offset=${offset}&limit=${limit}`)
     const data = await response.json()
     return data
 }
