c452f833e13f102b9860f539121387b987a5f79d
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-02 00:45:56 +0400

    feat: products
    created getProducts async action for products reducer

--- src/api/http-api.js
+++ src/api/http-api.js
@@ -5,23 +5,22 @@ export const fetchGet = async (endpoint) => {
         method: "GET",
     })
     const data = await response.json()
-    console.log(data);
     return data
 }
 
 
 export const createGetRequest = async (endpoint) => {
-    await fetchGet(endpoint)
+    return fetchGet(endpoint)
 }
 
 
 export const createGetRequsetWithParams = async (endpoint, offset, limit) => {
     const endpoints = `${endpoint}/?offset=${offset}&limit=${limit}`
-    fetchGet(endpoints)
+    return fetchGet(endpoints)
 }
 
 
 export const createGetById = async (endpoint, id) => {
     const endpoints = `${endpoint}/${id}`
-    fetchGet(endpoints)
+    return fetchGet(endpoints)
 }
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { productsSelector } from '../../redux/selectors/products';
+import { getProducts } from '../../redux/actions/asyncGetProducts';
+import { API_ENDPOINTS } from '../../constants/api';
 
 const Products = () => {
+    const dispatch = useDispatch()
+    const products = useSelector(productsSelector)
 
+    useEffect(() => {
+        dispatch(getProducts(API_ENDPOINTS.PRODUCTS))
+    }, [dispatch])
     return (
         <div>
-            Products
+            {/* {products.map(({ id, title }) =>
+                <h1 key={id}>{title}</h1>
+            )} */}
         </div>
     );
 };
