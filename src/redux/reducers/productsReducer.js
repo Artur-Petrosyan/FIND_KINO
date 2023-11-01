3f045feea98539934d4d72b6269cb448be320e41
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-02 00:19:21 +0400

    feat: productsReducer
    using GET_PRODUCTS type in the switch case

--- src/redux/reducers/productsReducer.js
+++ src/redux/reducers/productsReducer.js
@@ -1,5 +1,10 @@
+import { GET_PRODUCTS } from "../../constants/types";
+
 const productsReducer = (state = [], { type, data }) => {
-      switch(type) {
-        case 
-      }
+    switch (type) {
+        case GET_PRODUCTS: return [...data]
+        default: return state
+    }
 }
+
+export default productsReducer;
