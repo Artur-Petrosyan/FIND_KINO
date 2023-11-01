675b3fcf31b8943078dc6eac9a90ae4864ed5cf5
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-01 23:53:35 +0400

    feat: App
    imported pages,and created Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import Header from "./components/Header/Header";
 import { Routes, Route } from 'react-router-dom'
 import '../src/styles/reset.scss'
 import '../src/styles/common.scss'
+import Home from "./pages/Home/Home";
+import SingleProduct from "./pages/SingleProduct/SingleProduct";
+import Products from "./pages/Products/Products";
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} /> */}
-          {/* <Route path="products/:id" element={<SingleProduct />} /> */}
-
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="products/:id" element={<SingleProduct />} />
       </Routes>
 
     </div>
