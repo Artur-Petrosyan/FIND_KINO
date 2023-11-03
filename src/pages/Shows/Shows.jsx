d5f0f4d33513e456c8f208c80132d15693b723b2
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-04 00:04:45 +0400

    feat: Single Product
    created the navigateWithRegex function for navigation to single product page

--- src/pages/Shows/Shows.jsx
+++ src/pages/Shows/Shows.jsx
@@ -5,27 +5,39 @@ import { getShowes } from '../../redux/actions/asyncGetShowes';
 import { API_ENDPOINTS } from '../../constants/api';
 import Pagination from '../../components/Pagination/Pagination';
 import usePagination from '../../hooks/usePagination'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import  navigateToRegex  from '../../utils/navigateToRegex.js';
+
 const Showes = () => {
     const params = useParams()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
     const [pageNumber, setPageNumber] = useState(params.page)
 
-    const dispatch = useDispatch()
     const shows = useSelector(showsSelector)
     const sliced = usePagination(shows, pageNumber)
+    const navigateWithRegex = (id, name) => {
+        const newName = navigateToRegex(name)
+        return navigate(`${id}/${newName}`)
+    }
+
+
     useEffect(() => {
         dispatch(getShowes(API_ENDPOINTS.SHOWS))
-       
     }, [dispatch])
+
 
     return (
         <div style={{ display: 'flex', flexWrap: 'wrap' }}>
             {sliced.map(({ id, image, name }) =>
-                <div key={id} style={{ margin: 20 }}>
-                    <img src={image.medium} alt="" />
+                <div key={id} style={{ margin: 20 }} onClick={() => {
+                    navigateWithRegex(id,name)
+                }}>
+                    <img src={image.medium} alt="img" />
                 </div>
             )}
-            <Pagination shows={shows} pageNumber={pageNumber}  setPageNumber={setPageNumber}/>
+            <Pagination shows={shows} pageNumber={pageNumber} setPageNumber={setPageNumber} />
         </div>
     );
 };
