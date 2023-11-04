757fadf0cef6dc4fcfaa8cc334ce26bdcc542c70
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-04 19:57:22 +0400

    feat: singleShows
    using the component Card into SingleShow

--- src/redux/reducers/singleShowReducer.js
+++ src/redux/reducers/singleShowReducer.js
@@ -2,7 +2,7 @@ import { SET_SINGLE_SHOW } from "../../constants/types";
 
 const singleShowReducer = (state = [], { type, data }) => {
     switch (type) {
-        case SET_SINGLE_SHOW: return data;
+        case SET_SINGLE_SHOW: return [data];
         default: return state
     }
 }
