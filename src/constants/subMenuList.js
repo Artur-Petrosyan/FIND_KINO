76046d1253e26a73326eb28144076b22ebcd41dd
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-05 12:05:25 +0400

    feat: SingleShow
    created SubMenuList constant

---
+++ src/constants/subMenuList.js
@@ -0,0 +1,38 @@
+const subMenuList = [
+    {
+        id : 1,
+        MAIN : 'Main'
+    },
+    {
+        id : 2,
+        MAIN : 'Episodes'
+    },
+
+    {
+        id : 3,
+        MAIN : 'Seasons'
+    },
+    {
+        id : 4,
+        MAIN : 'Cast'
+    },
+    {
+        id : 5,
+        MAIN : 'Crew'
+    },
+    {
+        id : 6,
+        MAIN : 'Characters'
+    },
+    {
+        id : 7,
+        MAIN : 'Gallery'
+    },
+    {
+        id : 8,
+        MAIN : 'News'
+    },
+
+]
+
+export default subMenuList
