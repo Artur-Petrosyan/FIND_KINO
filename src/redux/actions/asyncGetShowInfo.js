5a8caab5322b1512ae9eeed0756153010da5a176
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-06 18:21:41 +0400

    feat: asyncAction
    changed the logic into asyncGetShowInfo,and created the type GET_SHOW_INFO

--- src/redux/actions/asyncGetShowInfo.js
+++ src/redux/actions/asyncGetShowInfo.js
@@ -1,15 +1,12 @@
 import { createGetRequestShowInfo } from "../../api/http-api";
-import { GET_SHOW_INFO_EPISODES, GET_SHOW_INFO_SEASONS } from "../../constants/types"
+import { GET_SHOW_INFO } from "../../constants/types"
 
 export const getShowInfo = (endpoint, id, info) => async (dispatch) => {
     console.log(info);
     try {
         const data = await createGetRequestShowInfo(endpoint, id, info)
-        switch (info) {
-            case 'episodes': return dispatch({ type: GET_SHOW_INFO_EPISODES, data: data })
-            case 'seasons': return dispatch({ type: GET_SHOW_INFO_SEASONS, data: data })
-            default: return 
-        }
+        dispatch({ type: GET_SHOW_INFO, data: data, info })
+
     }
     catch (err) {
         console.log(err);
