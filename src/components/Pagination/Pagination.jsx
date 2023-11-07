4d2059feb410d44c9000b9fe2225a8eda98ae70d
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-07 17:59:11 +0400

    feat: Pagination
    added the arrowleft and arrowRigth icons for change the page

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,35 +1,57 @@
 import Button from "../Button/Button";
+import toLeft from '../../assets/icons/toLeftArrow.svg'
+import toRight from '../../assets/icons/toRightArrow.svg'
 
 import { memo } from "react";
 import { useNavigate } from "react-router-dom";
 
 import '../../styles/pagination.scss'
 
-const Pagination = memo(({ shows, setPageNumber, pageNumber }) => {
+const Pagination = memo(({ data, setPageNumber, pageNumber, pageName }) => {
     const navigate = useNavigate()
     let numOfPages = []
 
-    let pages = Math.ceil(shows.length / 18)
-
+    let pages = Math.ceil(data.length / 25)
     for (let i = 1; i <= pages; i++) {
         numOfPages.push(i)
     }
     return (
         <div className='pagination' >
+            <div onClick={() => {
+                if (pageNumber >= 2) {
+                    navigate(`/${pageName}/${Number(pageNumber - 1)}`);
+                    setPageNumber(Number(pageNumber) - 1);
+                }
+            }}>
+                <img className="pagination__arrow" src={toLeft} alt="left" />
+            </div>
             {numOfPages.map((num) =>
                 <div key={num} className={`pagination__number  ${pageNumber === num ? 'active' : ''}`} >
+
                     <Button
                         variant="pagination__button"
                         onClick={() => {
                             if (num === pageNumber) return
-                            navigate(`/shows/${Number(num)}`);
+                            navigate(`/${pageName}/${Number(num)}`);
                             setPageNumber(num);
                         }}
                     >
                         {num}
                     </Button>
+
                 </div>
+
             )}
+            <div onClick={() => {
+                if (pageNumber < numOfPages.length) {
+                    console.log(pageNumber);
+                    navigate(`/${pageName}/${Number(pageNumber + 1)}`);
+                    setPageNumber(Number(pageNumber) + 1);
+                }
+
+            }}>
+                <img className="pagination__arrow" src={toRight} alt="right" />
+            </div>
         </div>
     );
 });
