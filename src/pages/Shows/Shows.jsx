216a79d08e67fc4c4d17e8b9b485fec9d732c4af
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-08 18:21:10 +0400

    feat: loader
    using the Loader in Showes page

--- src/pages/Shows/Shows.jsx
+++ src/pages/Shows/Shows.jsx
@@ -8,24 +8,29 @@ import '../../styles/shows.scss'
 import useSlicedData from '../../hooks/useSlicedData.js';
 import { API_ENDPOINTS } from '../../constants/api.js';
 import { GET_SHOWS } from '../../constants/types.js';
+import Loader from '../../components/Loader/Loader.jsx';
 
 const Showes = () => {
-    const { setPageNumber, navigateWithRegex, sliced, data, pageNumber } = useSlicedData(API_ENDPOINTS.SHOWS, 'shows', GET_SHOWS)
+    const { setPageNumber, navigateWithRegex, sliced, data, pageNumber, isLoading } = useSlicedData(API_ENDPOINTS.SHOWS, 'shows', GET_SHOWS)
     return (
         <div className='content'>
-            <div className='shows'>
-                {sliced.map(({ id, image, name, rating }) =>
-                    <Card
-                        key={id}
-                        id={id}
-                        name={name}
-                        image={image?.original}
-                        navigateWithRegex={navigateWithRegex}
-                        rating={rating?.average}
-                    />
-                )}
-            </div>
-            <Pagination data={data} pageNumber={pageNumber} pageName='shows' setPageNumber={setPageNumber} />
+            {isLoading ? <Loader /> :
+                <>
+                    <div className='shows'>
+                        {sliced.map(({ id, image, name, rating }) =>
+                            <Card
+                                key={id}
+                                id={id}
+                                name={name}
+                                image={image?.original}
+                                navigateWithRegex={navigateWithRegex}
+                                rating={rating?.average}
+                            />
+                        )}
+                    </div>
+                    <Pagination data={data} pageNumber={pageNumber} pageName='shows' setPageNumber={setPageNumber} />
+                </>
+            }
         </div>
     );
 };
