7a0a614e4ef610ecfa1ad0ff08c140b59357c150
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-12 00:06:18 +0400

    feat: input Search
    created the Input component and using antd Input

---
+++ src/components/InputSearch/InputSearch.jsx
@@ -0,0 +1,24 @@
+import { createGetRequestSearch } from '../../api/http-api'
+import { useState } from 'react'
+import { Input } from 'antd'
+const { Search } = Input
+
+
+const InputSearch = () => {
+
+    const [searchQuery, setSearchQuery] = useState('')
+    return (
+        <div className='header__search'>
+            <Search
+                placeholder="Find show"
+                allowClear
+                enterButton="Search"
+                onChange={(e) => setSearchQuery(e.target.value)}
+                size="large"
+
+            />
+        </div>
+    )
+}
+
+export default InputSearch
