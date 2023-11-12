0abb41fb8bb9a89a6b92066c9ca7e04588d40785
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-12 20:10:22 +0400

    feat: People page
    using the Select component for filtre peoples by gender and created the filtredByOption in utils

--- src/pages/People/People.jsx
+++ src/pages/People/People.jsx
@@ -3,9 +3,11 @@ import { GET_PEOPLES } from "../../constants/types";
 import Card from "../../components/Card/Card";
 import Pagination from "../../components/Pagination/Pagination";
 import Loader from "../../components/Loader/Loader";
-import { Row, Col } from 'antd'
+import { Row, Col, Select } from 'antd'
 import useSlicedData from "../../hooks/useSlicedData";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { filtredByOption } from "../../utils/filtredByOption";
 const People = () => {
     const { page } = useParams()
     const {
@@ -16,9 +18,22 @@ const People = () => {
         isLoading
     } = useSlicedData(API_ENDPOINTS.PEOPLES, 'peoples', GET_PEOPLES, page)
 
+    const selectOptions = [
+        { value: "All Persons", label: "All Persons" },
+        { value: 'Male', label: 'Male' },
+        { value: 'Female', label: 'Female' },
+
+    ]
+
+    const [filtredByGender, setFiltredByGender] = useState([])
     return (
         <div className='content'>
             {isLoading ? <Loader /> : <>
+                <Select
+                    options={selectOptions}
+                    onChange={(gender) => filtredByOption(gender,data,setFiltredByGender) }
+                    defaultValue='All Persons'
+                />
                 <Row gutter={20} wrap={true}>
                     {sliced.map(({ id, image, name, rating }) =>
                         <Col key={id} xl={3} lg={6} md={8} sm={12} xs={24} span={3} className='row__col'>
