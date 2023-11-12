74e370390c2da6956152d912bf99b9c9caeec86e
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-12 16:58:08 +0400

    feat: location
    added the logic for navigate from Search page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,17 +3,20 @@ import Card from 'antd/es/card/Card';
 import '../../styles/card.scss'
 import Meta from 'antd/es/card/Meta';
 import { HeartOutlined, StarOutlined } from '@ant-design/icons'
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { navigateWithRegex } from '../../utils/navigateWithRegex';
 const _Card = ({ id, name, image, rating }) => {
     const navigate = useNavigate()
+    const location = useLocation()
+    const path = location.pathname
+
     return (
         <Card
             className='card'
             key={id}
             title={name}
             cover={image ? <img src={image} alt='img' className='card__image' /> : <span>Not image</span>}
-            onClick={() => navigateWithRegex && navigateWithRegex(id, name,navigate)}
+            onClick={() => navigateWithRegex && navigateWithRegex(id, name, navigate,path)}
             size='small'
         >
             <Meta title={name} />
