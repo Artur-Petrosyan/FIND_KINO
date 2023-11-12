fda74731db0be3c9b17b43151508e8158887e97a
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-12 15:42:53 +0400

    feat: grid antd
    using Row and Col in searchPage

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -4,6 +4,9 @@ import { useLocation } from 'react-router-dom'
 import { searchShowsSelector } from '../../redux/selectors/searchShows';
 import getSearchShows from '../../redux/actions/asyncGetSearchShows';
 import Card from '../../components/Card/Card';
+import { Row, Col } from 'antd';
+import '../../styles/searchPage.scss'
+
 const SearchPage = () => {
     const location = useLocation()
     const search = new URLSearchParams(location.search)
@@ -15,20 +18,27 @@ const SearchPage = () => {
     }, [query, dispatch])
     return (
         <div>
-            {searchShows ? searchShows.map(({ show }) => {
-                const {id,name,image,rating} = show
-                return (
-                    <Card
-                        key={id}
-                        id={id}
-                        name={name}
-                        image={image?.original}
-                        // navigateWithRegex={navigateWithRegex}
-                        rating={rating?.average}
-                    />
-                )
-            }
-            )
+            {searchShows ?
+                <Row gutter={20} wrap={true}>
+                    {
+                        searchShows.map(({ show }) => {
+                            const { id, name, image, rating } = show
+                            return (
+                                <Col key={id} xl={3} lg={6} md={8} sm={12} xs={24} span={3} className='row__col'>
+                                    <Card
+                                        key={id}
+                                        id={id}
+                                        name={name}
+                                        image={image?.original}
+                                        // navigateWithRegex={navigateWithRegex}
+                                        rating={rating?.average}
+                                    />
+                                </Col>
+                            )
+                        }
+                        )
+                    }
+                </Row>
                 : <p>Not Items</p>}
         </div>
     );
