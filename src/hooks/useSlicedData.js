32c481d46e566b855d11a509a4bc3e1048fa4dc3
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-12 16:48:21 +0400

    style: imports
    removed unneccesary imports in useSlicedData

--- src/hooks/useSlicedData.js
+++ src/hooks/useSlicedData.js
@@ -1,9 +1,7 @@
 import { useEffect, useMemo, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
 import { dataSelector } from "../redux/selectors/shows"
 import sliceForPagination from "./usePagination"
-import navigateToRegex from "../utils/navigateToRegex"
 import { setPageAction } from "../redux/actions/setPage"
 import { getData } from "../redux/actions/asyncGetData"
 
