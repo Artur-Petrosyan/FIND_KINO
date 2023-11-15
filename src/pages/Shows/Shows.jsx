51b48c5707597c9bd154901ea20bd60621fc6978
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-15 23:59:31 +0400

    feat(Shows): combine
    combine the business and ui in Shows page

--- src/pages/Shows/Shows.jsx
+++ src/pages/Shows/Shows.jsx
@@ -1,52 +1,10 @@
-import React from 'react';
-
-import { GET_SHOWS } from 'src/constants/types.js';
-import { API_ENDPOINTS } from 'src/constants/api.js';
-
-import Pagination from 'components/Pagination/Pagination';
-import Card from 'components/Card/Card.jsx';
-import Loader from 'components/Loader/Loader.jsx';
-
-import useSlicedData from 'src/hooks/useSlicedData.js';
-
 import 'src/styles/shows.scss'
-import { useParams } from 'react-router-dom';
-import { Row, Col } from 'antd'
+import {useShowsPageViewModel} from "./model/useShowsPageViewModel";
+import {ShowesPagePure} from "./ui/components/ShowesPagePure";
 
-const Showes = () => {
-    const { page } = useParams()
-    const {
-        setPageNumber,
-        sliced,
-        data,
-        pageNumber,
-        isLoading
-    } = useSlicedData(API_ENDPOINTS.SHOWS, 'shows', GET_SHOWS, page)
-
-
-    return (
-        <div className='content'>
-            {isLoading ? <Loader /> :
-                <>
-                    <Row gutter={20} wrap={true}>
-                        {sliced.map(({ id, image, name, rating }) =>
-                            <Col key={id} xl={3} lg={6} md={8} sm={12} xs={24} span={3} className='row__col'>
-                                <Card
-                                    key={id}
-                                    id={id}
-                                    name={name}
-                                    image={image?.original}
-                                    rating={rating?.average}
-                                />
-                            </Col>
-                        )}
-                    </Row>
-
-                    <Pagination data={data} pageNumber={pageNumber} pageName='shows' setPageNumber={setPageNumber} />
-                </>
-            }
-        </div>
-    );
+const Shows = () => {
+    const {setPageNumber, sliced, data, pageNumber, isLoading} = useShowsPageViewModel();
+    return ShowesPagePure(isLoading, sliced, data, pageNumber, setPageNumber);
 };
 
-export default Showes;
+export default Shows;
