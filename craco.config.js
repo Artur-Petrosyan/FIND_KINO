e7bcd820ade223808ba02f6e426f3d8398b45b41
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-15 22:15:51 +0400

    feat(craco): config
    create the craco config file and using the alias for src

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+import path from "path";
+
+const resolvePath = (p) => path.resolve(__dirname,p)
+
+module.exports = {
+    webpack : {
+        port : '3000',
+        alias : {
+            src : resolvePath('./src')
+        }
+    }
+}
