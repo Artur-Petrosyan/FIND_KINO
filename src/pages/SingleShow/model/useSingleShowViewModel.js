8826f8a0b8ca66bd397a2a7f33b7a6063c71a72a
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-16 20:25:29 +0400

    feat(SingleShow): model
    create the model folder and remove business logic from `Shows` to `model`

---
+++ src/pages/SingleShow/model/useSingleShowViewModel.js
@@ -0,0 +1,21 @@
+import {useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
+import {useNavigate, useParams} from "react-router-dom";
+import {getSingleShow} from "src/redux/actions/asyncSingleShow";
+import {API_ENDPOINTS} from "src/constants/api";
+
+export function useSingleShowViewModel() {
+    const [isLoading, setIsLoading] = useState(true)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const navigateTo = (name) => {
+        const path = name.toLowerCase()
+        navigate(`${path}`)
+    }
+    const {id} = useParams()
+
+    useEffect(() => {
+        dispatch(getSingleShow(API_ENDPOINTS.SHOWS, id, setIsLoading))
+    }, [dispatch, id])
+    return {isLoading, navigateTo};
+}
