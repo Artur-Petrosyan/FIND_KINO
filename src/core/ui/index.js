91e9e4171f6cf74a7ffd70ad12a77a52b6119aef
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-16 20:19:01 +0400

    feat(core/ui): exports
    export the components

--- src/core/ui/index.js
+++ src/core/ui/index.js
@@ -1,1 +1,7 @@
-/*TODO: move components/ here after webpack alias creation with craco*/
+export {Button} from './components/Button'
+export {Card} from './components/Card'
+export {Header} from './components/Header'
+export {NavBar} from './components/NavBar'
+export {Loader} from './components/Loader'
+export {InputSearch} from './components/InputSearch'
+
