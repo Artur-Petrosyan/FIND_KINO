a3bea0a41ef349ba050e83b73ee439578d642d87
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-16 20:27:19 +0400

    feat(Shows): ui , model
    using the ui and model in Shows

--- src/pages/SingleShow/SingleShow.jsx
+++ src/pages/SingleShow/SingleShow.jsx
@@ -1,42 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
-import { getSingleShow } from 'src/redux/actions/asyncSingleShow';
-import { API_ENDPOINTS } from 'src/constants/api';
-
-
-import NavBar from 'components/NavBar/NavBar';
-import subMenuList from 'src/constants/subMenuList';
-import SingleShowInfo from 'src/pages/SingleShowInfo/SIngleShowInfo';
-import Loader from 'components/Loader/Loader';
+import {useSingleShowViewModel} from "./model";
+import {SingleShowPure} from "./ui/components";
 
 
 const SingleShow = () => {
-    const [isLoading, setIsLoading] = useState(true)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const navigateTo = (name) => {
-        const path = name.toLowerCase()
-        navigate(`${path}`)
-    }
-    const { id } = useParams()
-
-    useEffect(() => {
-        dispatch(getSingleShow(API_ENDPOINTS.SHOWS, id, setIsLoading))
-    }, [dispatch, id])
-    return (
-        <div >
-            {isLoading ? <Loader /> :
-                <>
-                    <div className='nav__bar'>
-                        <NavBar navList={subMenuList} subMenu={true} navigateTo={navigateTo} />
-                    </div>
-                    <div>
-                        <SingleShowInfo />
-                    </div>
-                </>}
-        </div >
-    );
+    const {isLoading, navigateTo} = useSingleShowViewModel();
+    return <SingleShowPure
+        isLoading={isLoading}
+        navigateTo={navigateTo}
+    />;
 };
 
 export default SingleShow;
