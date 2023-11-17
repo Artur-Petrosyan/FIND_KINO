53d0529b2873570dd60139ec3df723ec328cf46a
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-17 22:47:30 +0400

    fix(post): name
    change the `get` to `post`

--- src/core/lib/http-api/fetchPostRequest.js
+++ src/core/lib/http-api/fetchPostRequest.js
@@ -1,14 +1,14 @@
 import {BASE_URL} from "src/constants/api";
 
-export const fetchGetRequest = async (endpoint,data) => {
+export const fetchPostRequest = async (endpoint,data) => {
     try {
         let baseUrl = `${BASE_URL}`;
         const response = await fetch(`${baseUrl}${endpoint}`, {
             headers : {
                 "Content-Type": "application/json",
             },
-            method: "POST",
             body : JSON.stringify(data),
+            method: "POST",
         })
         return await response.json()
     } catch (err) {
