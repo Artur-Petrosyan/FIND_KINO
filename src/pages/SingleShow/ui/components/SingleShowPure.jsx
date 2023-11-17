369d921648a163cac335fed65fcd3b7a95fded73
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-17 21:50:14 +0400

    refactor: path
    change the path

--- src/pages/SingleShow/ui/components/SingleShowPure.jsx
+++ src/pages/SingleShow/ui/components/SingleShowPure.jsx
@@ -1,6 +1,6 @@
 import {Loader, NavBar} from "src/core";
 import subMenuList from "src/constants/subMenuList";
-import SingleShowInfo from "src/pages/SingleShowInfo/SIngleShowInfo";
+import SingleShowInfo from "src/pages/SingleShowInfo/ui/SIngleShowInfo";
 import React from "react";
 
 export function SingleShowPure({isLoading, navigateTo}) {
