74c46cf239f3c89587c4c3216e1ca0040443ca92
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-17 22:48:20 +0400

    feat: http-api
    use the `fetchGetRequest`

--- src/api/http-api.js
+++ src/api/http-api.js
@@ -1,35 +1,23 @@
-import { BASE_URL } from "src/constants/api"
+import {fetchGetRequest} from "../core/lib/http-api/fetchGetRequest";
 
-export const fetchGet = async (endpoint) => {
-    try {
-        const response = await fetch(`${BASE_URL}${endpoint}`, {
-            method: "GET",
-        })
-        const data = await response.json()
-        return data
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
 
 
 export const createGetRequest = async (endpoint) => {
-    return fetchGet(endpoint)
+    return fetchGetRequest(endpoint)
 }
 
 export const createGetRequestForSingleShow = async (endpoint, id) => {
     const endpoints = `${endpoint}/${id}`
-    return fetchGet(endpoints)
+    return fetchGetRequest(endpoints)
 }
 
 export const createGetRequestShowInfo = async (endpoint, id, info) => {
     const endpoints = `${endpoint}/${id}/${info}`
-    return fetchGet(endpoints)
+    return fetchGetRequest(endpoints)
 }
 
 
 export const createGetRequestSearch = async (searchQuery) => {
 const endpoint = `search/shows?q=${searchQuery}`
-return fetchGet(endpoint)
+return fetchGetRequest(endpoint)
 }
