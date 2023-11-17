f634a0c99a0b02cefa08c07c18440710b1ef5910
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-17 22:50:11 +0400

    feat: JSDoc
    added the `JS doc`

--- src/core/lib/http-api/fetchGetRequest.js
+++ src/core/lib/http-api/fetchGetRequest.js
@@ -1,13 +1,18 @@
-import {BASE_URL} from "src/constants/api";
 
+/**
+ * Fetches a GET request to the specified endpoint.
+ *
+ * @param {string} endpoint - The endpoint to send the GET request to.
+ * @returns {Promise<object>} - A Promise that resolves to the response data.
+ */
 export const fetchGetRequest = async (endpoint) => {
     try {
-        let baseUrl = `${BASE_URL}`;
+        const baseUrl = `${BASE_URL}`;
         const response = await fetch(`${baseUrl}${endpoint}`, {
             method: "GET",
-        })
-        return await response.json()
+        });
+        return await response.json();
     } catch (err) {
         console.log(err);
     }
-}
+};
--- src/core/lib/http-api/fetchPostRequest.js
+++ src/core/lib/http-api/fetchPostRequest.js
@@ -1,17 +1,23 @@
-import {BASE_URL} from "src/constants/api";
 
-export const fetchPostRequest = async (endpoint,data) => {
+/**
+ * Fetches a POST request to the specified endpoint with the provided data.
+ *
+ * @param {string} endpoint - The endpoint to send the POST request to.
+ * @param {object} data - The data to send in the request body.
+ * @returns {Promise<object>} - A Promise that resolves to the response data.
+ */
+export const fetchPostRequest = async (endpoint, data) => {
     try {
-        let baseUrl = `${BASE_URL}`;
+        const baseUrl = `${BASE_URL}`;
         const response = await fetch(`${baseUrl}${endpoint}`, {
-            headers : {
+            headers: {
                 "Content-Type": "application/json",
             },
-            body : JSON.stringify(data),
+            body: JSON.stringify(data),
             method: "POST",
-        })
-        return await response.json()
+        });
+        return await response.json();
     } catch (err) {
         console.log(err);
     }
-}
+};
