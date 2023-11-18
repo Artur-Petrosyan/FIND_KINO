35a0893e0fab59e7d0c717e7f3c50f520e35e4cf
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-18 20:35:04 +0400

    refactor(ui): ui
    create the headless `ShowSeasonsPure` component

---
+++ src/pages/ShowSeasons/ui/components/ShowSeasonsPure.jsx
@@ -0,0 +1,14 @@
+export const ShowSeasonsPure = (showInfo, info) => (
+    <div>
+        {showInfo[info].map(({id, image, number}) => <div key={id} style={{display: 'flex'}}>
+            <div>
+                <img src={image?.medium} alt=""/>
+            </div>
+            <div>
+                <h1>
+                    Season {number}
+                </h1>
+            </div>
+        </div>)}
+    </div>
+);
