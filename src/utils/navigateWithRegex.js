ca6b8f2426852b8cb37eea0819e44cbbe211c132
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-18 19:58:38 +0400

    refactor(utils): delete

--- src/utils/navigateWithRegex.js
+++
@@ -1,9 +0,0 @@
-import navigateToRegex from "./navigateToRegex"
-
-export const navigateWithRegex = (id, name, navigate,path) => {
-    const newName = navigateToRegex(name)
-    if(path === '/search') {
-      return  navigate(`/shows/1/${id}/${newName}/main`)
-    }
-    return navigate(`${id}/${newName}/main`)
-}
