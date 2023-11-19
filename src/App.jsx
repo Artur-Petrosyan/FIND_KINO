9d8eba5e6f325146489918885b1f40a81cfd7af8
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-19 18:55:19 +0400

    feat(lazyLoader): HOC
    create the `HOC` for lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
-import { Routes, Route } from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 
-import Home from "pages/Home/Home";
+
 import {Header} from "src/core";
-import Shows from "pages/Shows/ui/Shows";
-import PageNotFound from "pages/PageNotFound/PageNotFound";
-import SingleShow from "pages/SingleShow/ui/SingleShow";
-import SingleShowInfo from "pages/SingleShowInfo/ui/SIngleShowInfo";
-import People from "pages/People/ui/People";
-import SearchPage from "pages/SearchPage/ui/SearchPage";
-
+import {lazy} from "react";
 import './styles/common.scss'
 import './styles/reset.scss'
+import {LazyLoadWrapper} from "./core/lib/lazyLoadWrapper";
 
+const Home = lazy(() => import('pages/Home/Home'))
+const Shows = lazy(() => import('pages/Shows/ui/Shows'))
+const PageNotFound = lazy(() => import('pages/PageNotFound/PageNotFound'))
+const SingleShow = lazy(() => import('pages/SingleShow/ui/SingleShow'))
+const SingleShowInfo = lazy(() => import('pages/SingleShowInfo/ui/SIngleShowInfo'))
+const People = lazy(() => import('pages/People/ui/People'))
+const SearchPage = lazy(() => import('pages/SearchPage/ui/SearchPage'))
 
 const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path='search' element={<SearchPage />} />
-          <Route path='shows/:page' element={<Shows />} />
-          <Route path='shows/:page/:id/:name' element={<SingleShow />} >
-            <Route path=':info' element={<SingleShowInfo />} />
-          </Route>
-          <Route path="people/:page" element={<People />} />
+    return (
+        <div className="App">
+            <Header/>
+            <div className="container">
+                <Routes>
+                    <Route index={true} element={LazyLoadWrapper(<Home/>)}/>
+                    <Route path='search' element={LazyLoadWrapper(<SearchPage/>)}/>
+                    <Route path='shows/:page' element={LazyLoadWrapper(<Shows/>)}/>
+                    <Route path='shows/:page/:id/:name' element={LazyLoadWrapper(<SingleShow/>)}>
+                        <Route path=':info' element={LazyLoadWrapper(<SingleShowInfo/>)}/>
+                    </Route>
+                    <Route path="people/:page" element={LazyLoadWrapper(<People/>)}/>
+                    <Route path='*' element={LazyLoadWrapper(<PageNotFound/>)}/>
+                </Routes>
+            </div>
+        </div>
 
-          <Route path='*' element={<PageNotFound />} />
-
-        </Routes>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
