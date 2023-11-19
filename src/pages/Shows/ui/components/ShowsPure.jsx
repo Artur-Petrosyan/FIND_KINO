46451920a34f3d635b98c7a2bf424375a2f66486
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-19 19:04:50 +0400

    refactor: props
    remove the `isLoading` prop

--- src/pages/Shows/ui/components/ShowsPure.jsx
+++ src/pages/Shows/ui/components/ShowsPure.jsx
@@ -1,14 +1,11 @@
 import {Col, Row} from "antd";
 import React from "react";
-import {Loader} from "src/core/ui";
 import {Card} from "src/core/ui/components/Card";
 import {Pagination} from "src/core/ui";
 import './shows.scss'
 
-export const ShowsPure = (isLoading, sliced, data, pageNumber, setPageNumber) => (
+export const ShowsPure = ({setPageNumber, sliced, data, pageNumber}) => (
     <div className='content'>
-        {isLoading ? <Loader/> :
-            <>
                 <Row gutter={20} wrap={true}>
                     {sliced.map(({id, image, name, rating}) =>
                         <Col key={id} xl={3} lg={6} md={8} sm={12} xs={24} span={3} className='row__col'>
@@ -24,7 +21,5 @@ export const ShowsPure = (isLoading, sliced, data, pageNumber, setPageNumber) =>
                 </Row>
 
                 <Pagination data={data} pageNumber={pageNumber} pageName='shows' setPageNumber={setPageNumber}/>
-            </>
-        }
     </div>
 );
