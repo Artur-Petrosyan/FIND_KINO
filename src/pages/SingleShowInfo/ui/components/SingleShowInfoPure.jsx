aa460b7479f9f01df75a120dce454359d13c737d
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-19 19:07:50 +0400

    feat: scope
    use the scope

--- src/pages/SingleShowInfo/ui/components/SingleShowInfoPure.jsx
+++ src/pages/SingleShowInfo/ui/components/SingleShowInfoPure.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Loader} from "src/core";
 import './singleShow.scss'
 
-export const SingleShowInfoPure = (isLoading, renderComponent) => (
+export const SingleShowInfoPure = ({isLoading, renderComponent}) => (
     <div>
         {isLoading ? <Loader/> : renderComponent}
     </div>
