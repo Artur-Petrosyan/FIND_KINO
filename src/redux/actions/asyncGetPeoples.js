81d31ac9137fa30b7cf1183a4f2d16f8b9da3e31
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-11-26 14:06:58 +0400

    feat(api): http-request
    use the `EazyRequest` class
    for request

--- src/redux/actions/asyncGetPeoples.js
+++ src/redux/actions/asyncGetPeoples.js
@@ -1,8 +1,9 @@
-import { createGetRequest } from "src/api/http-api"
 import { GET_PEOPLES } from "src/constants/types"
-
+import {EazyRequest} from "eazyrequest";
+import {BASE_URL} from "src/constants/api";
+const api = new EazyRequest(BASE_URL)
 export const getPeoples = (endpoint, setIsLoading) => async (dispatch) => {
-    const data = await createGetRequest(endpoint)
+    const data = await api.get(endpoint)
     dispatch({ type: GET_PEOPLES, data: data })
     setIsLoading(false)
 }
--- src/redux/actions/asyncSingleShow.js
+++ src/redux/actions/asyncSingleShow.js
@@ -1,13 +1,13 @@
-import { createGetRequestForSingleShow } from "src/api/http-api"
 import { SET_SINGLE_SHOW } from "src/constants/types"
-
+import {EazyRequest} from "eazyrequest";
+import {BASE_URL} from "src/constants/api";
+const api = new EazyRequest(BASE_URL)
 export const getSingleShow = (enpoint, id,setIsloading) => async (dispatch) => {
     try {
         setIsloading(true)
-        const data = await createGetRequestForSingleShow(enpoint, id)
+        const data = await api.get(`${enpoint}/${id}`)
         dispatch({ type: SET_SINGLE_SHOW, data: data })
         setIsloading(false)
-
     }
     catch (err) {
         console.log(err);
